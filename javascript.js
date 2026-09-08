7210cb9a43986a7f230cfbc5336fd04b6825f6ba
abhijeetjais2025-droid <abhijeetjais2025@gmail.com>
2026-09-08 08:40:22 +0545

    Java Script

--- javascript.js
+++ javascript.js
@@ -6,3 +6,19 @@
  
  document.getElementById("result").innerHTML = "Result: " + sum;
     }
+    function subtract(){
+ let number1 = Number(document.getElementById("number 1").value);
+ let number2 = Number(document.getElementById("number 2").value);
+
+ let difference = number1 - number2;
+
+ document.getElementById("result").innerHTML = "Result: " + difference;
+    }
+    function multiply(){
+ let number1 = Number(document.getElementById("number 1").value);
+ let number2 = Number(document.getElementById("number 2").value);
+
+ let product = number1 * number2;
+
+ document.getElementById("result").innerHTML = "Result: " + product;
+    }
